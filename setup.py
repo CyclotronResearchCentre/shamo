453916c3ef74845416515330e4c8e417a4be9681
Martin <mar.grignard@gmail.com>
2020-03-18 10:35:29 +0100

    [CI/CD] Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme_file:
-    long_description = readme_file.read()
+long_description = open("README.md", "r").read()
+
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering :: Medical Science Apps."
+]
 
 setup(
-    name='shamo',
-    version='v1.0.0',
+    name="shamo",
+    version="v1.0.0",
     license="GPLv3",
-    description='Forward modelling and sensitivity analysis.',
+    description="Forward modelling and sensitivity analysis.",
     long_description=long_description,
-    author='Martin Grignard',
-    author_email='mar.grignard@uliege.be',
+    author="Martin Grignard",
+    author_email="mar.grignard@uliege.be",
     packages=find_packages(),
-    install_requires=["numpy", "nibabel", "scipy", "scikit-learn"]
+    install_requires=["numpy", "nibabel", "scipy", "scikit-learn"],
+    classifiers=CLASSIFIERS,
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"]
 )
