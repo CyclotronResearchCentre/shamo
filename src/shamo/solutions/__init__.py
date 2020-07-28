81d6cd37759002ca2bb038f4e8679f893b3c0139
Martin Grignard <mar.grignard@gmail.com>
2020-07-28 11:26:56 +0200

    [FEATURE] Add 'MaternProd' kernel

--- src/shamo/solutions/__init__.py
+++ src/shamo/solutions/__init__.py
@@ -1,6 +1,11 @@
 """API for `shamo.solutions`."""
 from .forward.common_forward_solution import CommonForwardSolution
 from .forward.forward_solution import ForwardSolution
-from .forward.parametric_forward_solution import ParametricForwardSolution
+from .forward.parametric_forward_solution import MaternProd, ParametricForwardSolution
 
-__all__ = ["CommonForwardSolution", "ForwardSolution", "ParametricForwardSolution"]
+__all__ = [
+    "CommonForwardSolution",
+    "ForwardSolution",
+    "MaternProd",
+    "ParametricForwardSolution",
+]
