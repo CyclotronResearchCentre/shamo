04b07a11f8fd0a46cde9c53650c4f2b8b76e1218
Martin <mar.grignard@gmail.Com>
2021-01-25 08:41:34 +0100

    [FEATURE] Added mask in surrogate

--- src/shamo/core/surrogate/masked_scalar.py
+++ src/shamo/core/surrogate/masked_scalar.py
@@ -38,6 +38,51 @@ class SurrMaskedScalar(SurrScalar):
         self.update({"suffix": kwargs.get("suffix", "")})
 
     @classmethod
+    def fit(cls, name, parent_path, sol, **kwargs):
+        """Generate a Gaussian process from a set of training data.
+
+        Parameters
+        ----------
+        name : str
+            The name of the generated surrogate model.
+        parent_path : str, byte or os.PathLike
+            The path to the parent directory of the surrogate model.
+        sol : shamo.core.solutions.parametric.SolParamABC
+            The parametric solution to generate a surrogate model for.
+
+        Returns
+        -------
+        shamo.Surrogate
+            The generated surrogate model.
+
+        Other Parameters
+        ----------------
+        kernel : sklearn.gaussian_process.kernels.Kernel
+            The kernel used to generate the Gaussian process.
+        n_restarts_optimizer : int
+            The number of restarts for the optimisation step.
+        random_state : int
+            The seed for the random state.
+        alpha : float
+            The added diagonal to account for noise on training points.
+        n_proc : int
+            The number of jobs to run in parallel. ``None`` means 1 job runs at a time
+            and ``-1`` means all cores are used.
+        metric : func
+            The metric to compute.
+        suffix : str
+            The suffix to append to the file query.
+
+        See Also
+        --------
+        sklearn.gaussian_process.GaussianProcessRegressor
+        """
+        surr = super().fit(name, parent_path, sol, **kwargs)
+        surr["suffix"] = kwargs.get("suffix", "")
+        surr.save()
+        return surr
+
+    @classmethod
     def _get_data(cls, sol, suffix="", metric=None, mask=None, **kwargs):
         """Extract relevant data from a parametric solution.
 
@@ -126,6 +171,54 @@ class SurrMaskedScalarNii(SurrMaskedScalar):
     --------
     shamo.core.surrogate.SurrMaskedScalar
     """
+
+    @classmethod
+    def fit(cls, name, parent_path, sol, **kwargs):
+        """Generate a Gaussian process from a set of training data.
+
+        Parameters
+        ----------
+        name : str
+            The name of the generated surrogate model.
+        parent_path : str, byte or os.PathLike
+            The path to the parent directory of the surrogate model.
+        sol : shamo.core.solutions.parametric.SolParamABC
+            The parametric solution to generate a surrogate model for.
+
+        Returns
+        -------
+        shamo.Surrogate
+            The generated surrogate model.
+
+        Other Parameters
+        ----------------
+        kernel : sklearn.gaussian_process.kernels.Kernel
+            The kernel used to generate the Gaussian process.
+        n_restarts_optimizer : int
+            The number of restarts for the optimisation step.
+        random_state : int
+            The seed for the random state.
+        alpha : float
+            The added diagonal to account for noise on training points.
+        n_proc : int
+            The number of jobs to run in parallel. ``None`` means 1 job runs at a time
+            and ``-1`` means all cores are used.
+        metric : func
+            The metric to compute.
+        suffix : str
+            The suffix to append to the file query.
+        mask : nibabel.Nifti1Image
+            A NIFTI image to use as a mask.
+
+        See Also
+        --------
+        sklearn.gaussian_process.GaussianProcessRegressor
+        """
+        mask = kwargs.pop("mask")
+        data = mask.get_fdata().astype(bool)
+        surr = super().fit(name, parent_path, sol, mask=data, **kwargs)
+        mask.to_filename(surr.path / f"{surr.name}_mask.nii")
+        return surr
 
     @classmethod
     def _get_masked_data(cls, sol, suffix, mask):
