3e381d8b3fa3baed8771895c665f22e4af402e97
Martin <mar.grignard@gmail.Com>
2021-01-26 17:14:30 +0100

    [FEATURE] Added tDCS NII surrogate models

--- src/shamo/hd_tdcs/__init__.py
+++ src/shamo/hd_tdcs/__init__.py
@@ -4,6 +4,19 @@ from .simulation.single.solution import SolHDTDCSSim
 
 from .simulation.parametric.problem import ProbParamHDTDCSSim
 from .simulation.parametric.solution import SolParamHDTDCSSim
+from .simulation.parametric.surrogate import (
+    SurrMaskedScalarNiiJ,
+    SurrMaskedScalarNiiMagJ,
+    SurrMaskedScalarNiiV,
+)
 
 
-__all__ = ["ProbHDTDCSSim", "SolHDTDCSSim", "ProbParamHDTDCSSim", "SolParamHDTDCSSim"]
+__all__ = [
+    "ProbHDTDCSSim",
+    "SolHDTDCSSim",
+    "ProbParamHDTDCSSim",
+    "SolParamHDTDCSSim",
+    "SurrMaskedScalarNiiJ",
+    "SurrMaskedScalarNiiMagJ",
+    "SurrMaskedScalarNiiV",
+]
