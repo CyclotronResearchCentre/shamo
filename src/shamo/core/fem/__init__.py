d8e97684c27ac4552e6312e564537a8a850f01a7
Martin <mar.grignard@gmail.Com>
2021-01-27 10:42:50 +0100

    [FEATURE] Added SurfSensorABC and CircleSensor

--- src/shamo/core/fem/__init__.py
+++ src/shamo/core/fem/__init__.py
@@ -4,7 +4,17 @@ from .field import Field
 from .tissue import Tissue
 from .sensors.abc import SensorABC
 from .sensors.point import PointSensor
+from .sensors.surface import SurfSensorABC, CircleSensor
 
 from .fem import FEM
 
-__all__ = ["FEM", "Field", "Group", "Tissue", "SensorABC", "PointSensor"]
+__all__ = [
+    "FEM",
+    "Field",
+    "Group",
+    "Tissue",
+    "SensorABC",
+    "PointSensor",
+    "SurfSensorABC",
+    "CircleSensor",
+]
