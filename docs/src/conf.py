7629f114143176072518b49106ab7c40b7f19f3a
Martin <mar.grignard@gmail.Com>
2021-04-22 15:27:32 +0200

    [DOC] Updated copyright date

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath("../../src"))
 # -- Project information -----------------------------------------------------
 
 project = "shamo"
-copyright = "2020, GIGA CRC In-Vivo Imaging"
+copyright = "2020-2021, GIGA CRC In-Vivo Imaging"
 author = "Martin Grignard <mar.grignard@uliege.be>"
 
 # The full version, including alpha/beta/rc tags
