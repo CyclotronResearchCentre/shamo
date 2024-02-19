fix: :bug: Fix broadcast error
Fixed broadcast error between affine and shape and empty list. This comparison was used to check if either the affine matrix or the shape vector were empty. Now, the size attribute is used.

src/shamo/core/problems/single/components/grid_sampler.py
@@ -43,7 +43,9 @@ class CompGridSampler(CompABC):
         bool
             Return ``True`` if the grid can be used, ``False`` otherwise.
         """
-        return True if self["affine"] != [] and self["shape"] != [] else False
+        has_affine = np.array(self["affine"]).size != 0
+        has_shape = np.array(self["shape"]).size != 0
+        return has_affine and has_shape
 
     @property
     def affine(self):